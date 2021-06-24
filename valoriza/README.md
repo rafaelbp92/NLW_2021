77c18b14591a5328822b73ef366fda510c2e7f0c
Rafael Pantoja <rafaelbp92@gmail.com>
2021-06-24 17:02:43 -0300

    Adding migrations, services and controllers

---
+++ valoriza/README.md
@@ -0,0 +1,28 @@
+# NLW_2021
+
+#NodeJS Project created during the Next Level Week Together 2021 event
+
+##Regras
+
+-Cadastro de Usuario
+
+[ ] Nao e permitido cadastrar mais de um usuario com o mesmo email
+
+[ ] Nao e permitido cadastrar usuario sem email
+
+-Cadastro de TAG
+
+[ ] Nao e permitido cadastrar mais de uma TAG com o mesmo nome
+
+[ ] Nao e permitido cadastrar TAG sem nome
+
+[ ] Nao e permitido cadastro de usuarios que nao sejam administradores
+
+-Cadastro de elogios
+
+[ ] Nao e permitido um usuario cadastrar um elogio para si proprio
+
+[ ] Nao e  permitido cadastrar elogios para usuarios invalidos
+
+[ ] O usuario precisa estar autenticado na aplicacao 
+
---
+++ valoriza/src/database/migrations/1624544737994-CreateUsers.ts
@@ -0,0 +1,47 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1624544737994 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "Users",
+                columns: [
+                    {
+                        name: "Id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "admin",
+                        type: "bollean",
+                        default: false
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
