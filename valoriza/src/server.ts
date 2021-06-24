deaf3531db282b3eb4a5db67a6e809b5319c384a
Rafael Pantoja <rafaelbp92@gmail.com>
2021-06-24 08:11:21 -0300

    Initial migrations config

--- valoriza/src/server.ts
+++ valoriza/src/server.ts
@@ -1,14 +1,9 @@
+import "reflect-metadata";
 import express, { request, response } from "express";
+import "./database"
 
-//@types/express
 const app = express();
 
-app.get("/teste", (request, response) => {
-    return response.send("Olá NLW");
-});
 
-app.post("/testePost", (request, response) => {
-    return response.send("Olá POST");
-})
 
 app.listen(3000, () => console.log("Server is running"));
